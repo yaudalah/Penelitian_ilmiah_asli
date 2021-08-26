c5afe75808d35541734157034e7b8f3e097c79f3
yaudalah <hardisyafrizal@gmail.com>
2021-08-26 22:51:41 +0700

    deleted and update img and codes

--- website/urls.py
+++ website/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import HomeView, ArticleDetailView, About
 
 urlpatterns = [
-    path('', HomeView.as_view(), name="home"),
+    # path('', HomeView.as_view(), name="home"),
+    path('', HomeView, name="home"),
     path('article/<int:pk>', ArticleDetailView.as_view(), name="Article-Detail"),
 ]
--- website/views.py
+++ website/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Post
+from django.core.paginator import Paginator
 
-class HomeView(ListView):
-    model = Post
-    template_name = 'home/index.html'
-    ordering = ['-date']
+def HomeView(request):
+    p = Paginator(Post.objects.all(), 3)
+    page = request.GET.get('page')
+    posts = p.get_page(page)
+
+    return render(request, 'home/index.html',{'posts': posts})
 
 class ArticleDetailView(DetailView):
     model = Post
